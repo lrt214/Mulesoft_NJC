3eb26b19e29f6caa73b59495f7f2945b8a3de60a
LAV TIWARI <65153906+lrt214@users.noreply.github.com>
2021-09-09 19:06:30 +0530

    Update Connect.java

--- Connect.java
+++ Connect.java
@@ -3,15 +3,13 @@ import java.sql.DriverManager;
 import java.sql.SQLException;  
    
 public class Connect {  
-     /** 
-     * Connect to a sample database 
-     */  
+   
     public static void connect() {  
         Connection conn = null;  
         try {  
-            // db parameters  
+             
             String url = "jdbc:sqlite:C:/sqlite/Fav_movie.db";  
-            // create a connection to the database  
+            
             conn = DriverManager.getConnection(url);  
               
             System.out.println("Connection to SQLite has been established.");  
@@ -28,9 +26,7 @@ public class Connect {
             }  
         }  
     }  
-    /** 
-     * @param args the command line arguments 
-     */  
+      
     public static void main(String[] args) {  
         connect();  
     }  
